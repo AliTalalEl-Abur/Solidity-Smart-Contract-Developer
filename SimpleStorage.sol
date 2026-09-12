61f0c4f6c87d7c72c2a4ea0213dd5a728b9dbc9b
AliTalalEl-Abur <ali549954@gmail.com>
2026-09-12 22:41:13 +0200

    mi primer contrato

---
+++ SimpleStorage.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19; //this is the solidity versions
+
+contract SimpleStorage {
+
+    uint256 public myfavoriteNumber; //0
+
+    //uint256[] listOfFavouriteNumbers; // [0, 78, 90]
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //dynamic array
+    Person[] public listOfPeople;//[]
+
+
+    //chelsea->232
+    mapping (string => uint256) public nameToFavoriteNumber; 
+
+    //static array
+    //Person[3] public listOfPeople;//[]
+
+    //Person public pat = Person({favoriteNumber:7, name:"Pat"});
+    //Person public mariah = Person({favoriteNumber:16, name:"mariah"});
+    //Person public jon = Person({favoriteNumber:12, name:"Jon"});
+
+    function store(uint256 _favoriteNumber) public{
+        myfavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+    }
+
+    //calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber)public{
+        listOfPeople.push(Person(_favoriteNumber,_name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
